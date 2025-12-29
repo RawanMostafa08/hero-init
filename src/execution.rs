64d335025db135ff136c17fffbb61a1d560af1cf
RawanMostafa08 <rawanmostafa401@gmail.com>
2025-12-29 16:07:04 +0000

    feat: added execution module

---
+++ src/execution.rs
@@ -0,0 +1,46 @@
+use std::collections::HashMap;
+use std::fs::{self, OpenOptions};
+use std::io::Result;
+use std::io::{self, Write};
+use std::os::unix::fs::PermissionsExt;
+use std::path::Path;
+use std::process::Command;
+
+// Run a shell command with specified environment variables
+pub fn run_cmd(command: &str, env_vars: HashMap<String, String>) -> Result<()> {
+    let mut cmd = Command::new("sh");
+    cmd.arg("-c").arg(command);
+
+    for (k, v) in env_vars {
+        cmd.env(k, v);
+    }
+
+    let status = cmd.status()?;
+
+    if !status.success() {
+        Err(io::Error::other("failed to execute command"))
+    } else {
+        Ok(())
+    }
+}
+
+// Write content to a file atomically with specified permissions
+pub fn write_file_atomic(path: &Path, content: &str, mode: u32) -> Result<()> {
+    let temp_path = path.with_extension("tmp");
+
+    {
+        let mut file = OpenOptions::new()
+            .write(true)
+            .create(true)
+            .truncate(true)
+            .open(&temp_path)?;
+
+        file.write_all(content.as_bytes())?;
+        file.sync_all()?;
+    }
+
+    fs::set_permissions(&temp_path, fs::Permissions::from_mode(mode))?;
+    fs::rename(temp_path, path)?;
+
+    Ok(())
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 pub mod config;
 pub mod discovery;
+pub mod execution;
 pub mod metadata;
 pub mod network;
 pub mod users;
